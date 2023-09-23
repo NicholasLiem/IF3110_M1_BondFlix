487d1bac3b05ee1dbba59e9cddc3a86644e65bba
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-09-23 17:25:18 +0700

    fix: user repository interface

--- src/Core/Application/Repositories/UserRepository.php
+++ src/Core/Application/Repositories/UserRepository.php
@@ -5,15 +5,11 @@ use Core\Domain\Entities\User;
 
 interface UserRepository
 {
-//    public function findById(int $id): User;
-//
-//    public function findByUsername(string $username): User;
 
     public function createUser(User $user): User;
 
     public function getUserByUsername(string $username): ?User;
 
-//    public function updateUser(User $user): User;
-//
-//    public function deleteUser(int $user_id);
+    public function updateUser(User $user): User;
+    public function deleteUserByUsername(int $username);
 }
