daff0652cdbb765cf1b6e334cdafa8c5fc6e1791
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-09-25 10:11:28 +0700

    fix: update redirect function to support data extraction

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,6 +6,12 @@ require_once BASE_PATH . '/Bootstrap/Autoloader.php';
 require_once BASE_PATH . '/services.php';
 require_once BASE_PATH . '/routes.php';
 
-function redirect($path) {
-    require_once BASE_PATH . '/public/view/' . $path . '.php';
+function redirect($path, $data = []) {
+    extract($data);
+
+    ob_start();
+    include BASE_PATH . '/public/view/' . $path . '.php';
+    $content = ob_get_clean();
+
+    echo $content;
 }
