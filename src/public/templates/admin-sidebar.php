3704003dfd1d4a224e0ddb3dacf56039d3c2b96c
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-09-30 21:14:36 +0700

    refactor: edit sidebar

--- src/public/templates/admin-sidebar.php
+++ src/public/templates/admin-sidebar.php
@@ -1,16 +1,15 @@
 <?php
 ?>
+
 <link rel="stylesheet" href="/public/css/admin-sidebar.css">
-<nav class="admin-sidebar">
+<nav class="admin-sidebar" id="sidebar">
     <ul>
-      <img src="/public/logo.png" alt="Bondflix logo" class="logo-img" />
-      <li><a href="">Users</a></li>
-      <li><a href="/admin/movies">Movies</a></li>
-      <li><a href="">Genre</a></li>
-      <li><a href="">Categories</a></li>
-      <li><a href="">Actors</a></li>
-      <li><a href="">Directors</a></li>
+        <li><a href="/admin"><img src="/public/logo.png" alt="Bondflix logo" class="logo-img"/></a></li>
+        <li><a href="#"><div class="sidebar-item">Users</div></a></li>
+        <li><a href="/admin/movies"><div class="sidebar-item">Movies</div></a></li>
+        <li><a href="#"><div class="sidebar-item">Genre</div></a></li>
+        <li><a href="#"><div class="sidebar-item">Categories</div></a></li>
+        <li><a href="#"><div class="sidebar-item">Actors</div></a></li>
+        <li><a href="#"><div class="sidebar-item">Directors</div></a></li>
     </ul>
 </nav>
-
-
