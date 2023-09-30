cdbc21df2a9add7d3290019cf0661ef41d4dc458
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-09-30 23:44:15 +0700

    feat: login page

--- src/public/view/login.php
+++ src/public/view/login.php
@@ -1,18 +1,20 @@
 <?php
-$pageTitle = 'Login Page';
-$stylesheet = '/public/css/register.css';
+$pageTitle = 'Login';
+$stylesheet = '/public/css/login.css';
 $script = 'login.js';
 include BASE_PATH . "/public/templates/header.php"
 ?>
 
 <body>
 <div class="login-container">
-    <h1>Login</h1>
+    <a href="/"><img src="/public/logo.png" alt="Bonflix Logo" class="logo"></a>
+    <h1>Sign In</h1>
     <form id="login-form">
         <input type="text" id="input-username" name="username" placeholder="Username" required>
         <input type="password" id="input-password" name="password" placeholder="Password" required>
         <input type="submit" value="Login">
     </form>
+    <a class="register-link" href="/register">Haven't registered yet?</a>
 </div>
 <script>
     document.getElementById('login-form').addEventListener('submit', submitLogin);
