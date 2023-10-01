3b3473ab9dced88ca9de927a952f6460e599b207
Haziq <haziqabiyyu@gmail.com>
2023-10-01 08:13:03 +0700

    refactor: move sidebar stylesheet links

--- src/public/view/admin.php
+++ src/public/view/admin.php
@@ -7,25 +7,11 @@
     $username = $_SESSION['username'];
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Admin page</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Mono&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-  <body>
-    <?php
-    include $adminSidebarTemplate
-    ?>
-    <main>
-      <p>Welcome to Admin Page, <?php echo $username ?>!</p>
-    </main>
-  </body>
-</html>
+<body>
+  <?php
+  include $adminSidebarTemplate
+  ?>
+  <main>
+    <p>Welcome to Admin Page, <?php echo $username ?>!</p>
+  </main>
+</body>
