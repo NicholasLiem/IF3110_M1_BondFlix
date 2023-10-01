4d31ad849ffb95bca326192a9b84889f15aa7c22
Haziq <haziqabiyyu@gmail.com>
2023-10-01 22:10:33 +0700

    feat: create content api handler classes

---
+++ src/Handler/Content/ContentActorHandler.php
@@ -0,0 +1,49 @@
+<?php
+namespace Handler\Content;
+
+use Exception;
+use Handler\BaseHandler;
+use Utils\Http\HttpStatusCode;
+use Utils\Response\Response;
+
+class ContentActorHandler extends BaseHandler {
+    
+    protected static $instance;
+    protected $service;
+
+    private function __construct($service)
+    {
+        parent::__construct($service);
+    }
+
+    public static function getInstance($container): ContentHandler {
+        if (!isset(self::$instance)) {
+            self::$instance = new static(
+                $container->resolve('contentService')
+            );
+        }
+        return self::$instance;
+    }
+
+    protected function get($params = null)
+    {
+        //TODO: implement
+    }
+
+    protected function post($params = null)
+    {
+        //TODO: implement
+
+    }
+
+    protected function put($params = null)
+    {
+        //TODO: implement
+
+    }
+
+    protected function delete($params = null)
+    {
+        //TODO: implement
+    }
+}
