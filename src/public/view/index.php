a43d6f6f0b5f2ecce5aa0633ebc74d5583bf55e3
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-10-01 13:02:39 +0700

    feat: index page, button

--- src/public/view/index.php
+++ src/public/view/index.php
@@ -1,4 +1,22 @@
 <?php
-
-echo "Home page";
+    $pageTitle = 'Landing Page';
+    include BASE_PATH . "/public/templates/header.php";
 ?>
+
+<link rel="stylesheet" href="/public/css/index.css">
+<body>
+<a href="/"><img src="/public/logo.png" alt="Bonflix Logo" class="logo"></a>
+<div class="centered-content">
+    <div class="container">
+        <h1>The biggest local and international hits. The best stories. All streaming here.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+    </div>
+    <div class="register-container">
+        <input type="text" id="input-username" name="username" placeholder="Email address or username">
+        <input type="submit" id="input-submit" class="submit-button" value="Get Started 〉">
+    </div>
+</div>
+<a href="/login" id="sign-in-button" class="submit-button">Sign In</a>
+<script src="/public/js/index.js"></script>
+</body>
--- src/public/view/login.php
+++ src/public/view/login.php
@@ -20,13 +20,15 @@ if (isset($_SESSION["user_id"])) {
 <body>
 <div class="login-container">
     <a href="/"><img src="/public/logo.png" alt="Bonflix Logo" class="logo"></a>
-    <h1>Sign In</h1>
-    <form id="login-form">
-        <input type="text" id="input-username" name="username" placeholder="Username" required>
-        <input type="password" id="input-password" name="password" placeholder="Password" required>
-        <input type="submit" value="Login">
-    </form>
-    <a class="register-link" href="/register">Haven't registered yet?</a>
+    <div class="overlay">
+        <h1>Sign In</h1>
+        <form id="login-form">
+            <input type="text" id="input-username" name="username" placeholder="Username" required>
+            <input type="password" id="input-password" name="password" placeholder="Password" required>
+            <input type="submit" value="Login">
+        </form>
+        <a class="register-link" href="/register">Haven't registered yet?</a>
+    </div>
 </div>
 <script>
     document.getElementById('login-form').addEventListener('submit', submitLogin);
