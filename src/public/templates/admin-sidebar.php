846e73cd0b510125852b22010391c4997bcb4ad9
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-10-02 22:57:54 +0700

    refactor: media management and fix on admin sidebar

--- src/public/templates/admin-sidebar.php
+++ src/public/templates/admin-sidebar.php
@@ -12,12 +12,9 @@
 <nav class="admin-sidebar" id="sidebar">
     <ul>
         <li><a href="/admin"><img src="/public/logo.png" alt="Bondflix logo" class="logo-img"/></a></li>
-        <li><a href="/admin/users"><div class="sidebar-item">Users</div></a></li>
-        <li><a href="/admin/movies"><div class="sidebar-item">Movies</div></a></li>
-        <li><a href="/admin/genres"><div class="sidebar-item">Genre</div></a></li>
-        <li><a href="#"><div class="sidebar-item">Categories</div></a></li>
-        <li><a href="#"><div class="sidebar-item">Actors</div></a></li>
-        <li><a href="#"><div class="sidebar-item">Directors</div></a></li>
+        <li><a href="/admin/users" ><div class="sidebar-item" id="users-item">Users</div></a></li>
+        <li><a href="/admin/movies" ><div class="sidebar-item" id="movies-item">Movies</div></a></li>
+        <li><a href="/admin/media/management" ><div class="sidebar-item" id="media-item">Media Management</div></a></li>
         <li><a onclick="logout()"><div class="sidebar-item">Logout</div></a></li>
     </ul>
 </nav>
