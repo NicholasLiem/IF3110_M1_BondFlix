be515097d33f0f135dec93d97d56eda3202ed90b
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-10-02 23:30:33 +0700

    fix: sidebar and content

--- src/public/templates/admin-sidebar.php
+++ src/public/templates/admin-sidebar.php
@@ -14,7 +14,7 @@
         <li><a href="/admin"><img src="/public/logo.png" alt="Bondflix logo" class="logo-img"/></a></li>
         <li><a href="/admin/users" ><div class="sidebar-item" id="users-item">Users</div></a></li>
         <li><a href="/admin/movies" ><div class="sidebar-item" id="movies-item">Movies</div></a></li>
-        <li><a href="/admin/media/management" ><div class="sidebar-item" id="media-item">Media Management</div></a></li>
+        <li><a href="/admin/media/management" ><div class="sidebar-item" id="media-item">Media</div></a></li>
         <li><a onclick="logout()"><div class="sidebar-item">Logout</div></a></li>
     </ul>
 </nav>
--- src/public/view/admin-media-management.php
+++ src/public/view/admin-media-management.php
@@ -9,8 +9,8 @@ $username = $_SESSION['username'];
 
 <link rel="stylesheet" href="/public/css/admin-page.css">
 <link rel="stylesheet" href="/public/css/admin-media-management.css">
-<?php include $adminSidebarTemplate ?>
 <body>
+<?php include $adminSidebarTemplate ?>
 <div class="content">
     <div class="container">
         <form>
@@ -23,10 +23,12 @@ $username = $_SESSION['username'];
         <form id="add-new-genre-form">
             <label>Add New Genre:</label>
             <input type="text" name="genre_name" id="input-genre" required>
-            <input type="submit" id= "submit-new-genre" value="Add Genre">
+            <input type="submit" id="submit-new-genre" value="Add Genre">
         </form>
     </div>
 </div>
+
+
 <script src="/public/js/admin-media-management.js"></script>
 <script>
     document.getElementById('add-new-genre-form').addEventListener('submit', addNewGenre);
