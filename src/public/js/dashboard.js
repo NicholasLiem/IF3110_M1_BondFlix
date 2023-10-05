3c9f738235d98d0893480d3f1cdda2302feffbf2
Haziq <haziqabiyyu@gmail.com>
2023-10-05 15:54:06 +0700

    feat: styling for user account page

--- src/public/js/dashboard.js
+++ src/public/js/dashboard.js
@@ -1,11 +1,22 @@
+const accountButton = document.querySelector(".account-button");
+const accountMenu = document.querySelector(".account-menu");
+
+accountButton.addEventListener("click", () => {
+    if (accountMenu.style.display === "block") {
+        accountMenu.style.display = "none";
+    } else {
+        accountMenu.style.display = "block";
+    }
+});
 
 async function logout() {
     try {
         const httpClient = new HttpClient();
-        const response = await httpClient.post('/api/auth/logout', null, false);
+        const response = await httpClient.post("/api/auth/logout", null, false);
         const json = JSON.parse(response.body);
         if (json.success) {
-            document.cookie = "PHPSESSID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            document.cookie =
+                "PHPSESSID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
             window.location.href = "/";
         } else {
         }
--- src/public/view/account.php
+++ src/public/view/account.php
@@ -23,7 +23,7 @@ include BASE_PATH . "/public/templates/header.php";
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Roboto:wght@700&display=swap" rel="stylesheet">
 
 <body>
-    <!-- TODO: tolong diextract navbarnya jadi tamplate -->
+    <!-- TODO: tolong diextract navbarnya jadi template -->
     <nav class="navbar">
         <div>
             <a href="/"><img class="logo" src="/public/logo.png" alt="Bondflix logo"></a>
@@ -53,29 +53,36 @@ include BASE_PATH . "/public/templates/header.php";
     <main>
         <div id="account-settings-container">
             <div><h1>Account Settings</h1></div>
-            <div>
-                <div><img src="/public/avatar.png" alt="profile-picture"></div>
+            <div id="account-settings-content">
+                <div>
+                    <img src="/public/avatar.png" alt="profile-picture" id="profile-picture">
+                    <button id="edit-profile-pic-button">
+                        <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"></path>
+                        </svg>
+                    </button>
+                </div>
                 <div>
                     <ul>
                         <li>
                             <label for="first-name">First Name</label>
-                            <input type="text" id="first-name" name="first-name">
+                            <input type="text" id="first-name" name="first-name" class="text-input" placeholder="Current First Name">
                         </li>
                         <li>
                             <label for="last-name">Last Name</label>
-                            <input type="text" id="last-name" name="last-name">
+                            <input type="text" id="last-name" name="last-name" class="text-input" placeholder="Current Last Name">
                         </li>
                         <li>
                             <label for="password">Password</label>
-                            <input type="password" id="password" name="password">
+                            <input type="password" id="password" name="password" class="text-input" placeholder="Change password">
                         </li>
                     </ul>
                 </div>
             </div>
-            <div>
-                <button>Save</button>
-                <button>Cancel</button>
-                </div>
+            <div id="account-settings-buttons">
+                <button id="save-button">SAVE</button>
+                <button id="cancel-button">CANCEL</button>
+            </div>
         </div>
     </main>
 </body>
